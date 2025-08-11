38ddcec0b26d4406d00a6366fc5795c604e243b4
Jilanwkolo <jilanwkolo1@gmail.com>
2025-08-11 15:09:10 +0700

    Create header_admin.php
    header admin

---
+++ admin/header_admin.php
@@ -0,0 +1,14 @@
+<header class="admin-header">
+    <div class="logo">
+        <a href="dashboard.php">CMS Admin</a>
+    </div>
+    <nav class="admin-nav">
+        <ul>
+            <li><a href="pages.php">Halaman</a></li>
+            <li><a href="posts.php">Postingan</a></li>
+            <li><a href="menus.php">Menu</a></li>
+            <li><a href="layout.php">Layout</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+</header>
